784f6c71f3dee89595fb425064845850c633c690
BrayanYes92338 <bycaballero0@soy.sena.edu.co>
2024-09-24 11:04:54 -0500

    Actualizacion de fronted

--- src/components/Riego.vue
+++ src/components/Riego.vue
@@ -30,17 +30,17 @@
           </q-select>
           <q-input outlined v-model="diasTransplante" use-input hide-selected fill-input input-debounce="0"
             class="q-my-md q-mx-md" label="dias de Transplante" type="tel" required pattern="[0-9]+" maxlength="999" />
-          <q-select outlined v-model="estadoFenológico" :options="['Germinación', 'Emergencia', 'Crecimiento Vegetativo', 'Desarrollo de Yemas/Floración', 'Fructificación','Maduración', 'Cosecha','Senescencia', 'Dormancia (en algunos cultivos)']" label="Seleccione el Estado Fenológico"
-            class="q-my-md q-mx-md" />
+          <q-select outlined v-model="estadoFenológico"
+            :options="['Germinación', 'Emergencia', 'Crecimiento Vegetativo', 'Desarrollo de Yemas/Floración', 'Fructificación', 'Maduración', 'Cosecha', 'Senescencia', 'Dormancia (en algunos cultivos)']"
+            label="Seleccione el Estado Fenológico" class="q-my-md q-mx-md" />
           <q-input outlined v-model="horaInicio" use-input hide-selected fill-input input-debounce="0"
             class="q-my-md q-mx-md" label="hora Inicio" type="time" />
           <q-input outlined v-model="horaFin" use-input hide-selected fill-input input-debounce="0"
             class="q-my-md q-mx-md" label="hora Fin" type="time" />
           <q-input outlined v-model="dosis" use-input hide-selected fill-input input-debounce="0"
-            class="q-my-md q-mx-md" label="dosis (litro/hora)" type="tel" required pattern="[0-9]+" maxlength="999"/>
+            class="q-my-md q-mx-md" label="dosis (litro/hora)" type="tel" required pattern="[0-9]+" maxlength="999" />
           <q-input outlined v-model="cantidadAgua" use-input hide-selected fill-input input-debounce="0"
-            class="q-my-md q-mx-md" label="cantidad de Agua" type="tel" 
-            />
+            class="q-my-md q-mx-md" label="cantidad de Agua" type="tel" />
           <q-card-actions align="right">
             <q-btn v-if="accion === 1" @click="validarIngresoRiego()" color="red" class="text-white"
               :loading="useRiego.loading">Agregar
@@ -302,9 +302,9 @@ function filtrarFn(val, update, abort) {
 }
 
 async function listarRiegos() {
- const r = await useRiego.listarRiegos()
- rows.value = r.data.riegos.reverse()
- console.log(r.data.riegos)
+  const r = await useRiego.listarRiegos()
+  rows.value = r.data.riegos.reverse()
+  console.log(r.data.riegos)
 }
 
 function validarIngresoRiego() {
@@ -316,7 +316,7 @@ function validarIngresoRiego() {
   } else if (diasTransplante.value == "" || diasTransplante.value.trim().length === 0) {
     Notify.create("Se debe agregar dias Transplante");
   } else if (!validacionnumeros.test(diasTransplante.value)) {
-      Notify.create("Los dias de Transplante solo debe llevar numeros"); 
+    Notify.create("Los dias de Transplante solo debe llevar numeros");
   } else if (estadoFenológico.value == "" || estadoFenológico.value.trim().length === 0) {
     Notify.create("Se debe agregar estado Fenológico");
   } else if (horaInicio.value == "" || horaInicio.value.trim().length === 0) {
@@ -324,13 +324,13 @@ function validarIngresoRiego() {
   } else if (horaFin.value == "" || horaFin.value.trim().length === 0) {
     Notify.create("Se debe agregar hora Final");
   } else if (dosis.value == "") {
-    Notify.create("Se debe agregar dosis"); 
+    Notify.create("Se debe agregar dosis");
   } else if (!validacionnumeros.test(dosis.value)) {
-    Notify.create("La dosis solo debe llevar numeros");  
+    Notify.create("La dosis solo debe llevar numeros");
   } else if (cantidadAgua.value == "") {
     Notify.create("Se debe agregar cantidad de Agua");
   } else if (!validacionnumeros.test(cantidadAgua.value)) {
-    Notify.create("La cantidad de Agua solo debe llevar numeros");  
+    Notify.create("La cantidad de Agua solo debe llevar numeros");
   } else {
     agregarRiego();
     Limpiar();
@@ -392,7 +392,7 @@ function validarEdicionRiego() {
   } else if (diasTransplante.value == "" || diasTransplante.value.trim().length === 0) {
     Notify.create("Se debe agregar dias Transplante");
   } else if (!validacionnumeros.test(diasTransplante.value)) {
-      Notify.create("Los dias de Transplante solo debe llevar numeros"); 
+    Notify.create("Los dias de Transplante solo debe llevar numeros");
   } else if (estadoFenológico.value == "" || estadoFenológico.value.trim().length === 0) {
     Notify.create("Se debe agregar estado Fenológico");
   } else if (horaInicio.value == "" || horaInicio.value.trim().length === 0) {
@@ -400,14 +400,14 @@ function validarEdicionRiego() {
   } else if (horaFin.value == "" || horaFin.value.trim().length === 0) {
     Notify.create("Se debe agregar hora Final");
   } else if (dosis.value == "") {
-    Notify.create("Se debe agregar dosis"); 
+    Notify.create("Se debe agregar dosis");
   } else if (!validacionnumeros.test(dosis.value)) {
-    Notify.create("La dosis solo debe llevar numeros");  
+    Notify.create("La dosis solo debe llevar numeros");
   } else if (cantidadAgua.value == "") {
     Notify.create("Se debe agregar cantidad de Agua");
   } else if (!validacionnumeros.test(cantidadAgua.value)) {
-    Notify.create("La cantidad de Agua solo debe llevar numeros");  
-  } else  {
+    Notify.create("La cantidad de Agua solo debe llevar numeros");
+  } else {
     editarRiego();
     Limpiar();
     cerrar();

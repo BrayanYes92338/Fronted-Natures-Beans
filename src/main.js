61cc8d7d765c58e359c7eea3e73dbbd854973452
BrayanYes92338 <bycaballero0@soy.sena.edu.co>
2024-10-21 13:24:58 -0500

    Actualizacion final del Sena Natures Beans

--- src/main.js
+++ src/main.js
@@ -15,11 +15,11 @@ import './style.css'
 
 //  <----Sitio para poner link generado de Glith ---->
 
-// axios.defaults.baseURL = "https://three-delirious-sock.glitch.me/"
+axios.defaults.baseURL = "https://deep-celestial-witch.glitch.me"
 
 // <----Sitio para poner link local---->
 
- axios.defaults.baseURL = "http://localhost:4500/"
+//  axios.defaults.baseURL = "http://localhost:4500/"
 
 let requestsPending = 0;
 
